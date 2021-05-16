2358e4d15525948cbfc1b1a472ff0c0093cfb90a
Lucas Gomes Oliveira <lucasgomoliveira@gmail.com>
2021-05-16 07:09:43 -0300

    First Upload

---
+++ TFTFS/Controllers/UsuarioController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using TFTFS.Models;
+using Newtonsoft.Json;
+
+namespace TFTFS.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: UsuarioController
+        public ActionResult Index()
+        {
+            var model = new List<Usuario>();
+            string path = @"Usuarios.json";
+            if (!System.IO.File.Exists(path))
+                System.IO.File.WriteAllText(path, "[]");
+            var obj = JArray.Parse(System.IO.File.ReadAllText(path));
+            model = obj.ToObject<List<Usuario>>();
+            return View(model);
+        }
+
+        // GET: UsuarioController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsuarioController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Usuario model)
+        {
+            try
+            {
+                string path = @"Usuarios.json";
+                string dados = "[]";
+                if (System.IO.File.Exists(path))
+                {
+                    var obj = JArray.Parse(System.IO.File.ReadAllText(path));
+                    obj.Add(JToken.FromObject(model));
+                    dados = obj.ToString();
+                }
+                System.IO.File.WriteAllText(path, dados);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuarioController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuarioController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuarioController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TFTFS/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TFTFS.Models
+{
+    public class Usuario
+    {
+        public string Nome { get; set; } = string.Empty;
+        public decimal Valor { get; set; } = 0;
+
+        public Usuario() { }
+
+        public Usuario(string nome, decimal valor)
+        {
+            Nome = nome;
+            Valor = valor;
+        }
+    }
+}
