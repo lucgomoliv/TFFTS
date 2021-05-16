55af7f7ba9f774a795f608735a9e1b834da5bddb
AnaLuizaBarros <analuizagbarros@gmail.com>
2021-05-16 20:29:55 -0300

    metodos

---
+++ TFTFS/Models/Metodos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TFTFS.Models
+{
+    public class Metodos : Usuario
+    {
+        List<Usuario> us = new List<Usuario>();
+        public decimal ValorInserido { get; set; } = 0;
+        public Metodos() { }
+
+        public Metodos(decimal valorInserido)
+        {
+            ValorInserido = valorInserido;
+        }
+
+        public bool Deposito(decimal valor, string nome) {
+            var NomeUsuario = us.Where(x => x.Nome.Equals(nome));
+            if (NomeUsuario.Count() == 1) {
+                foreach (var item in NomeUsuario)
+                {
+                    item.Valor += valor;
+                }
+                return true;
+            }
+          return false;
+        }
+        public bool Saque(decimal valor, string nome)
+        {
+            var NomeUsuario = us.Where(x => x.Nome.Equals(nome));
+            if (NomeUsuario.Count() == 1) { 
+                foreach (var item in NomeUsuario)
+                {
+                    item.Valor -= valor;
+                }
+            }
+            return false;
+        }
+    }
+}
